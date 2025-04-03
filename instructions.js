abc3e29c639a777c024577267ac7c63b205f721b
Jude Woolls <jwoolls01@gmail.com>
2025-04-03 20:07:38 +0100

    add end game modal display functions

--- instructions.js
+++ instructions.js
@@ -8,3 +8,15 @@ document.addEventListener('DOMContentLoaded', () => {
             document.getElementById('instructions-modal').classList.toggle('show');
         });
     });
+
+// functions to display and close the end game modal
+// export them to be used in game.js
+function displayEndGameModal() {
+    document.getElementById('end-game-modal').classList.toggle('hidden');
+    document.getElementById('end-game-modal').classList.toggle('show');
+}
+// will be added to the close button in the modal
+function closeEndGameModal() {
+    document.getElementById('end-game-modal').classList.toggle('hidden');
+    document.getElementById('end-game-modal').classList.toggle('show');
+}
